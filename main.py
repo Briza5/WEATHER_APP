f1a94c4e5172bdcc059d1977703d0ff8bfc57c91
Briza5 <BrizaL555@gmail.com>
2025-10-28 15:15:11 +0100

    new home page and new app function with json output

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template # Import Flask class and render_templat
 
 app = Flask("Website") # Instantiate the Flask application to the variable app
 
-@app.route("/home") # Define the route for the home URL
+@app.route("/") # Define the route for the home URL
 def home(): # Define the home function
-    return render_template("tutorial.html") # Use render_template to render the HTML file
+    return render_template("home.html") # Use render_template to render the HTML file
 
-@app.route("/about/") # Define the route for the about URL
-def about(): # Define the about function
-    return render_template("about.html") # Use render_template to render the HTML file
+@app.route("/api/v1/<station>/<date>") # Define the route for the API with dynamic parameters station and date
+def about(station, date): # define the about function with parameters station and date
+    temperature = 23    # Example temperature value
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}  # return a JSON response with station, date, and temperature
 
 
 app.run(debug=True) # Run the Flask application in debug mode
